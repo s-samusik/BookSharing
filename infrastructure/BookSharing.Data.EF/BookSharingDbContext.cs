dbac4fecb3e5fb42f3589cf6df2e8ea83b7c7c42
Sergey Samusik <s.samusik@outlook.com>
2020-11-29 03:33:38 +0300

    Changed solution's structure

---
+++ infrastructure/BookSharing.Data.EF/BookSharingDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookSharing.Data.EF
+{
+    public class BookSharingDbContext : DbContext
+    {
+    }
+}
