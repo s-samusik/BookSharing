676f68822b8ee1d573404873dd75f8950e96ec3f
Sergey Samusik <s.samusik@outlook.com>
2020-12-03 00:23:26 +0300

    Updated DTO, DbContext and Migrations.

--- domain/BookSharing/Data/RentLocationAddressDto.cs
+++ domain/BookSharing/Data/RentLocationAddressDto.cs
@@ -7,5 +7,8 @@
         public string City { get; set; }
         public string Street { get; set; }
         public string Building { get; set; }
+
+        public virtual RentLocationDto RentLocation { get; set; }
+
     }
 }
--- infrastructure/BookSharing.Data.EF/Migrations/20201202211652_Updated.Designer.cs
+++ infrastructure/BookSharing.Data.EF/Migrations/20201202211652_Updated.Designer.cs
@@ -4,14 +4,16 @@ using BookSharing.Data.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BookSharing.Data.EF.Migrations
 {
     [DbContext(typeof(BookSharingDbContext))]
-    partial class BookSharingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201202211652_Updated")]
+    partial class Updated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
