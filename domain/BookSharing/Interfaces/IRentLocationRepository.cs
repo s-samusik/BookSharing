b4a7e4061f7a9e708018c380343ab595051f5161
Sergey Samusik <s.samusik@outlook.com>
2020-12-07 01:30:02 +0300

    Added RentLocationRepository

--- domain/BookSharing/Interfaces/IRentLocationRepository.cs
+++ domain/BookSharing/Interfaces/IRentLocationRepository.cs
@@ -8,7 +8,6 @@ namespace BookSharing.Interfaces
     {
         Task AddAsync(RentLocation location);
         Task UpdateAsync(RentLocation location);
-        Task DeleteAsync(RentLocation location);
         Task<RentLocation> GetByIdAsync(int id);
         Task<List<RentLocation>> GetAllAsync();
     }
---
+++ infrastructure/BookSharing.Data.EF/Repositories/RentLocationRepository.cs
@@ -0,0 +1,53 @@
+﻿using BookSharing.Interfaces;
+using BookSharing.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookSharing.Data.EF.Repositories
+{
+    class RentLocationRepository : IRentLocationRepository
+    {
+        private readonly DbContextFactory dbContextFactory;
+
+        public RentLocationRepository(DbContextFactory dbContextFactory)
+        {
+            this.dbContextFactory = dbContextFactory;
+        }
+
+        public async Task AddAsync(RentLocation location)
+        {
+            var context = dbContextFactory.Create(typeof(RentLocationRepository));
+            var dto = RentLocation.DtoFactory.Create(location.Name, location.Country, location.City,
+                                                     location.Street, location.Building);
+            context.RentLocations.Add(dto);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(RentLocation location)
+        {
+            var context = dbContextFactory.Create(typeof(RentLocationRepository));
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<RentLocation> GetByIdAsync(int id)
+        {
+            var context = dbContextFactory.Create(typeof(RentLocationRepository));
+            var dto = await context.RentLocations.FindAsync(id);
+
+            return RentLocation.Mapper.Map(dto);
+        }
+
+        public async Task<List<RentLocation>> GetAllAsync()
+        {
+            var context = dbContextFactory.Create(typeof(RentLocationRepository));
+
+            var dtos = await context.RentLocations
+                                    .AsNoTracking()
+                                    .ToListAsync();
+
+            return dtos.Select(RentLocation.Mapper.Map).ToList();
+        }
+    }
+}
