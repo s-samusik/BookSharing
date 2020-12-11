63361e87451b00c4e4ee348fa9ea3bfa54887860
Sergey Samusik <s.samusik@outlook.com>
2020-12-12 00:47:55 +0300

    Added "User" interface

---
+++ domain/BookSharing/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookSharing.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookSharing.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(User user);
+
+        Task<User> GetByIdAsync(int id);
+        Task<User> GetByQueryAsync(string query);
+        Task<UserType> GetUserTypeByQueryAsync(string query);
+        Task<List<User>> GetAllByTypeAsync(UserType type);
+    }
+}
