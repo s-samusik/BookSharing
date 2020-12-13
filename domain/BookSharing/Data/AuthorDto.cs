d938633e8c738d30a9f0e27749d828d0314ea472
Sergey Samusik <s.samusik@outlook.com>
2020-12-13 23:57:44 +0300

    Added Dtos (author, book, genre, publisher)

---
+++ domain/BookSharing/Data/AuthorDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BookSharing.Data
+{
+    public class AuthorDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<BookDto> Books { get; set; }
+    }
+}
