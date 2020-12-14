809fc225c654721ed4521ee9821c4368c746b9ef
Sergey Samusik <s.samusik@outlook.com>
2020-12-14 23:35:17 +0300

    Added "Book" interface

---
+++ domain/BookSharing/Interfaces/IBookRepository.cs
@@ -0,0 +1,19 @@
+﻿using BookSharing.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookSharing.Interfaces
+{
+    public interface IBookRepository
+    {
+        Task AddAsync(Book book);
+        Task UpdateAsync(Book book);
+        Task DeleteAsync(Book book);
+
+        Task<Book> GetByIdAsync(int id);
+        Task<List<Book>> GetAllByQueryAsync(string query);
+        Task<List<Genre>> GetAllBookGenres();
+        Task<Genre> GetBookGenreByQueryAsync(string query);
+        Task<List<Book>> GetAllByGenreAsync(Genre genre);
+    }
+}
