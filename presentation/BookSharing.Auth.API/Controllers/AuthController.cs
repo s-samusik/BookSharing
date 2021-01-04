b127c97e6fe3deff1fd2ac3b8bf1ae2a57987322
Sergey Samusik <s.samusik@outlook.com>
2021-01-05 02:39:51 +0300

    Added Auth project into presentation layer

---
+++ presentation/BookSharing.Auth.API/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookSharing.Auth.API.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        // GET: api/login/
+        [HttpGet("login")]
+        public ActionResult Login()
+        {
+            return Ok("ok");
+        }
+    }
+}
